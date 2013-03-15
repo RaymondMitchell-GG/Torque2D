20ac6e448fb30d5aee43dd5c96038fdcb0f256ca
Raymond Mitchell <raym@torquepowered.com>
2013-03-15 10:50:55 -0700

    Last commit missed the adds.  Committing now.

---
+++ modules/EmptyProject/gui/readme.txt
@@ -0,0 +1,1 @@
+Gui taml and scripts go here.
---
+++ modules/EmptyProject/main.cs
@@ -0,0 +1,17 @@
+
+
+function EmptyProject::create( %this )
+{
+    // Load project scripts here
+    
+	
+	// Do project create stuffs here
+}
+
+//-----------------------------------------------------------------------------
+
+function EmptyProject::destroy( %this )
+{
+
+}
+
